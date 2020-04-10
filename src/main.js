af295581e7c2fc1943291482f7d14bb21490edbf
Andy Tom <andrew.tom@colorado.edu>
2020-04-10 17:50:22 -0600

    Add functionality to display cards and clear inputs on save

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ var ideas = document.querySelector('.ideas');
 var saveButton = document.querySelector('.save-button');
 var titleInput = document.querySelector('.title-input');
 var bodyInput = document.querySelector('.body-input');
+var cardTitle = document.querySelector('.card-title');
+var cardBody = document.querySelector('.card-body');
+var ideaCardsSection = document.querySelector('.idea-cards');
+
+
+
+
 var ideasArray = [];
 
 
@@ -21,7 +28,28 @@ function openMenuHeader () {
     ideas.classList.toggle('darkness');
 }
 
+function displayCards() {
+  ideaCardsSection.insertAdjacentHTML('beforeend', `<section class="single-card">
+    <div class="card-header">
+      <img src="assets/star-active.svg" class="idea-star" alt="white-star">
+      <img src="assets/menu-close.svg" class="idea-close" alt="X button">
+    </div>
+    <h4 class="card-title">${titleInput.value}</h4>
+    <p class="card-body">${bodyInput.value}</p>
+    <div class="card-footer">
+      <img src="assets/comment.svg" class="idea-comment" alt="plus-symbol">Comment
+    </div>
+  </section>`);
+
+}
+function clearInputs() {
+  bodyInput.value = '';
+  titleInput.value = '';
+}
+
 function storeIdea(event) {
   event.preventDefault();
   ideasArray.push(new Idea({title: titleInput.value, body: bodyInput.value}))
+  displayCards();
+  clearInputs();
 }
