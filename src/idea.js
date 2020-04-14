9bd2e32633aa838beddb2d9bfced6213777b1f15
Bill Wilke <billwilke2@gmail.com>
2020-04-14 14:09:18 -0600

    Adds saveToStorage function and updates id and isStarred

--- src/idea.js
+++ src/idea.js
@@ -2,12 +2,14 @@ class Idea {
   constructor(ideaObject) {
     this.title = ideaObject.title;
     this.body = ideaObject.body;
-    this.id = Date.now();
-    this.isStarred = false;
+    this.id = ideaObject.id || Date.now();
+    this.isStarred = ideaObject.isStarred || false;
   }
 
   saveToStorage() {
-
+    var ideaToStore = ideasArray
+    var stringifiedArray = JSON.stringify(ideaToStore);
+    localStorage.setItem('ideasArray', stringifiedArray);
   }
 
   deleteFromStorage() {
@@ -17,6 +19,7 @@ class Idea {
   updateIdea() {
 
   }
+
   toggleStar() {
     this.isStarred = !this.isStarred;
   }
